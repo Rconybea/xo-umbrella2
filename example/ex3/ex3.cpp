5b80fe2d202205b2231e5e822fd7e0a2bf9d3cb3
Roland Conybeare <rconybeare@gmail.com>
2023-09-16 14:21:09 -0400

    nestlog: examples/ex3.cpp: minor tweaks

--- example/ex3/ex3.cpp
+++ example/ex3/ex3.cpp
@@ -21,19 +21,22 @@ fib(int n) {
 
 int
 main(int argc, char ** argv) {
+    //std::cerr << "0         1         2         3         4         5         6         7         8         9        10" << std::endl;
+    //std::cerr << "01234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890" << std::endl;
+
     log_config::min_log_level = log_level::info;
     log_config::time_enabled = true;
     log_config::time_local_flag = true;
     log_config::style = FS_Streamlined;
     log_config::indent_width = 4;
     log_config::max_indent_width = 30;
-    log_config::location_tab = 100;
+    log_config::location_tab = 80;
     log_config::encoding = CE_Xterm;
     log_config::function_entry_color = 69;
     log_config::function_exit_color = 70;
     log_config::code_location_color = 166;
 
-    int n = 9;
+    int n = 3;
 
     scope log(XO_ENTER0(info), ":n ", 4);
 
