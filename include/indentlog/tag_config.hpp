6a06c769c8324a5f106ef1147d51bd470ab0454a
Roland Conybeare <rconybeare@gmail.com>
2023-09-18 13:09:15 -0400

    indentlog: refactor: tag_config.tag_color uses color_spec

--- include/indentlog/tag_config.hpp
+++ include/indentlog/tag_config.hpp
@@ -9,24 +9,18 @@ namespace xo {
     /* Tag here b/c we want header-only library */
     template <typename Tag>
     struct tag_config_impl {
-        /* color encoding */
-        static color_encoding encoding;
         /* color to use for tags
          *    os << tag("foo", foovalue)
          * to produces output like
          *    :foo foovalue
          * with :foo using .tag_color
          */
-        static std::uint32_t tag_color;
+        static color_spec_type tag_color;
     }; /*tag_config_impl*/
 
     template <typename Tag>
-    color_encoding
-    tag_config_impl<Tag>::encoding = color_encoding::xterm;
-
-    template <typename Tag>
-    std::uint32_t
-    tag_config_impl<Tag>::tag_color = 245;
+    color_spec_type
+    tag_config_impl<Tag>::tag_color = color_spec_type::xterm(245);
 
     using tag_config = tag_config_impl<class tag_config_tag>;
 } /*namespace xo*/
