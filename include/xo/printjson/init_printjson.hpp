0f4f874587b1530fe7d30b6247d6068322646db5
Roland Conybeare <rconybeare@gmail.com>
2023-10-09 14:54:17 -0400

    printjson: indentation in .hpp

--- include/xo/printjson/init_printjson.hpp
+++ include/xo/printjson/init_printjson.hpp
@@ -8,21 +8,21 @@
 #include "xo/subsys/Subsystem.hpp"
 
 namespace xo {
-  /* tag to represent the printjson/ subsystem within ordered initialization */
-  enum S_printjson_tag {};
+    /* tag to represent the printjson/ subsystem within ordered initialization */
+    enum S_printjson_tag {};
 
-  /* Use:
-   *   // anywhere, to declare printjson dependency  e.g. at file scope
-   *   InitEvidence s_evidence = InitSubsys<S_printjson_tag>::require();
-   *
-   *   // from main(), though can resort to module initialization in a pybind11 library
-   *   Subsystem::initialize_all();
-   */
-  template<>
-  struct InitSubsys<S_printjson_tag> {
-    static void init();
-    static InitEvidence require();
-  };
+    /* Use:
+     *   // anywhere, to declare printjson dependency  e.g. at file scope
+     *   InitEvidence s_evidence = InitSubsys<S_printjson_tag>::require();
+     *
+     *   // from main(), though can resort to module initialization in a pybind11 library
+     *   Subsystem::initialize_all();
+     */
+    template<>
+    struct InitSubsys<S_printjson_tag> {
+        static void init();
+        static InitEvidence require();
+    };
 } /*namespace xo*/
 
 
