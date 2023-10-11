9a23b29c12d53f773d6e46a45e7b3e1f95633821
Roland Conybeare <rconybeare@gmail.com>
2023-10-11 18:43:04 -0400

    cosmetic: formatting

--- include/xo/webutil/Alist.hpp
+++ include/xo/webutil/Alist.hpp
@@ -10,34 +10,34 @@
 #include <string_view>
 
 namespace xo {
-  namespace web {
-    /* assocation list, maps strings to strings
-     * use this for arguments to dynamic-endpoint-callbacks
-     */
-    class Alist {
-    public:
-      Alist() = default;
-      
-      /* lookup association by name */
-      std::string_view lookup(std::string n) const {
-	for (auto const & ix : this->assoc_v_) {
-	  if (ix.first == n) {
-	    return ix.second;
-	  }
-	}
+    namespace web {
+        /* assocation list, maps strings to strings
+         * use this for arguments to dynamic-endpoint-callbacks
+         */
+        class Alist {
+        public:
+            Alist() = default;
 
-	return "";
-      } /*lookup*/
+            /* lookup association by name */
+            std::string_view lookup(std::string n) const {
+                for (auto const & ix : this->assoc_v_) {
+                    if (ix.first == n) {
+                        return ix.second;
+                    }
+                }
 
-      void push_back(std::string n, std::string v) {
-	this->assoc_v_.push_back(std::make_pair(std::move(n), std::move(v)));
-      }
+                return "";
+            } /*lookup*/
 
-    private:
-      std::vector<std::pair<std::string, std::string>> assoc_v_;
-    }; /*Alist*/
-    
-  } /*namespace web*/
+            void push_back(std::string n, std::string v) {
+                this->assoc_v_.push_back(std::make_pair(std::move(n), std::move(v)));
+            }
+
+        private:
+            std::vector<std::pair<std::string, std::string>> assoc_v_;
+        }; /*Alist*/
+
+    } /*namespace web*/
 } /*namespace xo*/
 
 /* end Alist.hpp */
--- include/xo/webutil/StreamEndpointDescr.hpp
+++ include/xo/webutil/StreamEndpointDescr.hpp
@@ -5,7 +5,7 @@
 
 #pragma once
 
-#include "web_util/Alist.hpp"
+#include "Alist.hpp"
 #include "callback/CallbackSet.hpp"
 #include "refcnt/Refcounted.hpp"
 #include "indentlog/print/tag.hpp"
@@ -13,66 +13,66 @@
 #include <functional>
 
 namespace xo {
-  namespace reactor { class AbstractSink; }
+    namespace reactor { class AbstractSink; }
 
-  namespace web {
-    /* a function that creates an event subscription */
-    using StreamSubscribeFn = std::function<fn::CallbackId (ref::rp<reactor::AbstractSink> const & ws_sink)>;
-    using StreamUnsubscribeFn = std::function<void (fn::CallbackId id)>;
+    namespace web {
+        /* a function that creates an event subscription */
+        using StreamSubscribeFn = std::function<fn::CallbackId (ref::rp<reactor::AbstractSink> const & ws_sink)>;
+        using StreamUnsubscribeFn = std::function<void (fn::CallbackId id)>;
 
-    /* describes a stream endpoint
-     * this comprises
-     * - a uri pattern (matches stream name)
-     * - a function that establishes subscription
-     *   (by attaching supplied WebsocketSink to an event source)
-     */
-    class StreamEndpointDescr {
-    public:
-      StreamEndpointDescr(std::string uri_pattern,
-              StreamSubscribeFn subscribe_fn,
-              StreamUnsubscribeFn unsubscribe_fn)
-    : uri_pattern_{std::move(uri_pattern)},
-      subscribe_fn_{std::move(subscribe_fn)},
-      unsubscribe_fn_{std::move(unsubscribe_fn)} {}
+        /* describes a stream endpoint
+         * this comprises
+         * - a uri pattern (matches stream name)
+         * - a function that establishes subscription
+         *   (by attaching supplied WebsocketSink to an event source)
+         */
+        class StreamEndpointDescr {
+        public:
+            StreamEndpointDescr(std::string uri_pattern,
+                                StreamSubscribeFn subscribe_fn,
+                                StreamUnsubscribeFn unsubscribe_fn)
+                : uri_pattern_{std::move(uri_pattern)},
+                  subscribe_fn_{std::move(subscribe_fn)},
+                  unsubscribe_fn_{std::move(unsubscribe_fn)} {}
 
-      std::string const & uri_pattern() const { return uri_pattern_; }
-      StreamSubscribeFn const & subscribe_fn() const { return subscribe_fn_; }
-      StreamUnsubscribeFn const & unsubscribe_fn() const { return unsubscribe_fn_; }
+            std::string const & uri_pattern() const { return uri_pattern_; }
+            StreamSubscribeFn const & subscribe_fn() const { return subscribe_fn_; }
+            StreamUnsubscribeFn const & unsubscribe_fn() const { return unsubscribe_fn_; }
 
-      void display(std::ostream & os) const {
-    using xo::xtag;
+            void display(std::ostream & os) const {
+                using xo::xtag;
 
-    os << "<StreamEndpointDescr" << xtag("uri_pattern", uri_pattern_) << ">";
-      } /*display*/
+                os << "<StreamEndpointDescr" << xtag("uri_pattern", uri_pattern_) << ">";
+            } /*display*/
 
-      std::string display_string() const { return xo::tostr(*this); }
+            std::string display_string() const { return xo::tostr(*this); }
 
-    private:
-      /* unique pattern in URI-space for this endpoint
-       * for example
-       *    .uri_pattern = /stem/${foo}/${bar}
-       * means this endpoint generates contents for uri's
-       *    /stem/apple/banana
-       *    /stem/aphid/green
-       * but not for
-       *    /stem/apple/banana/carrot
-       */
-      std::string uri_pattern_;
-      /* a function that subscribes to an event stream
-       * (by attaching a websocket sink)
-       */
-      StreamSubscribeFn subscribe_fn_;
-      /* reverses effect of a particular call to .subscribe_fn */
-      StreamUnsubscribeFn unsubscribe_fn_;
-    }; /*StreamEndpointDescr*/
+        private:
+            /* unique pattern in URI-space for this endpoint
+             * for example
+             *    .uri_pattern = /stem/${foo}/${bar}
+             * means this endpoint generates contents for uri's
+             *    /stem/apple/banana
+             *    /stem/aphid/green
+             * but not for
+             *    /stem/apple/banana/carrot
+             */
+            std::string uri_pattern_;
+            /* a function that subscribes to an event stream
+             * (by attaching a websocket sink)
+             */
+            StreamSubscribeFn subscribe_fn_;
+            /* reverses effect of a particular call to .subscribe_fn */
+            StreamUnsubscribeFn unsubscribe_fn_;
+        }; /*StreamEndpointDescr*/
 
-    inline std::ostream &
-    operator<<(std::ostream & os, StreamEndpointDescr const & x) {
-      x.display(os);
-      return os;
-    } /*operator<<*/
+        inline std::ostream &
+        operator<<(std::ostream & os, StreamEndpointDescr const & x) {
+            x.display(os);
+            return os;
+        } /*operator<<*/
 
-  } /*namespace web*/
+    } /*namespace web*/
 } /*namespace xo*/
 
 /* end StreamEndpointDescr.hpp */
