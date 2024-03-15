8f11a8e413e22e2c651d8fda388875e2a5915ef3
Roland Conybeare <rconybeare@gmail.com>
2024-03-15 19:25:48 -0400

    build: streamline CMAKE_MODULE_PATH interaction

---
+++ cmake/xo-bootstrap-macros.cmake
@@ -0,0 +1,12 @@
+if (("${CMAKE_MODULE_PATH}" STREQUAL "") OR ("${CMAKE_MODULE_PATH}" STREQUAL "prefix"))
+    # default to typical install location for xo-project-macros
+    set(CMAKE_MODULE_PATH ${CMAKE_INSTALL_PREFIX}/share/cmake)
+endif()
+
+message("-- CMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX}")
+message("-- CMAKE_MODULE_PATH=${CMAKE_MODULE_PATH}")
+
+# needs to have been installed somewhere on CMAKE_MODULE_PATH,
+# (e.g. from xo-cmake with the same value for CMAKE_INSTALL_PREFIX)
+#
+include(xo_macros/xo-project-macros)
