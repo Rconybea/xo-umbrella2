9e7aebd6dd8c74c7e637f0645e7067149ccdf2da
Roland Conybeare <rconybeare@gmail.com>
2024-03-29 14:33:41 -0400

    build: suppress bootstrap message in submodule build

--- cmake/xo-bootstrap-macros.cmake
+++ cmake/xo-bootstrap-macros.cmake
@@ -3,8 +3,10 @@ if (("${CMAKE_MODULE_PATH}" STREQUAL "") OR ("${CMAKE_MODULE_PATH}" STREQUAL "pr
     set(CMAKE_MODULE_PATH ${CMAKE_INSTALL_PREFIX}/share/cmake)
 endif()
 
-message("-- CMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX}")
-message("-- CMAKE_MODULE_PATH=${CMAKE_MODULE_PATH}")
+if (NOT XO_SUBMODULE_BUILD)
+    message("-- CMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX}")
+    message("-- CMAKE_MODULE_PATH=${CMAKE_MODULE_PATH}")
+endif()
 
 # needs to have been installed somewhere on CMAKE_MODULE_PATH,
 # (e.g. from xo-cmake with the same value for CMAKE_INSTALL_PREFIX)
