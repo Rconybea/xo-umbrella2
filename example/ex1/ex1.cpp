4d823595fed8c28a4b4c8d7975795ef7f6013c89
Roland Conybeare <rconybeare@gmail.com>
2024-04-05 01:36:09 -0400

    xo-unit: ex1: + static assert

--- example/ex1/ex1.cpp
+++ example/ex1/ex1.cpp
@@ -5,12 +5,16 @@
 
 int
 main () {
+    namespace u = xo::unit::units;
     namespace qty = xo::unit::qty;
+    using xo::unit::quantity;
     using namespace std;
 
     auto t = qty::milliseconds(10);
     auto m = qty::kilograms(2.5);
     auto a = m / (t*t);
+
+    static_assert(same_as<decltype(t), quantity<u::millisecond, int>>);
 
     cerr << "t: " << t << ", m: " << m << ", m.t^-2: " << a << endl;
 }
