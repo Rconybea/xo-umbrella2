ecb321f0f18bfc8084e333becb9a9433d64d5681
Roland Conybeare <rconybeare@gmail.com>
2024-04-16 21:37:48 -0400

    tidy: cleanup stray stringlit -> flatstring naming

--- docs/conf.py
+++ docs/conf.py
@@ -6,7 +6,7 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'xo stringlit documentation'
+project = 'xo flatstring documentation'
 copyright = '2024, Roland Conybeare'
 author = 'Roland Conybeare'
 
