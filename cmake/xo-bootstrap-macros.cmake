633bbd4047707af8f408e729516ec44cf8ece0af
Roland Conybeare <rconybeare@gmail.com>
2024-04-25 14:26:16 -0400

    xo-flatstring: build: streamline/tweak .cmake files

--- cmake/xo-bootstrap-macros.cmake
+++ cmake/xo-bootstrap-macros.cmake
@@ -1,12 +1,14 @@
 # ----------------------------------------------------------------
-# for example use
+# for example:
 #   $ PREFIX=/usr/local   # for example
 #   $ cmake -DCMAKE_MODULE_PATH=prefix -DCMAKE_INSTALL_PREFIX=$PREFIX -B .build
 #
-# will set
-#   CMAKE_MODULE_PATH = /usr/local/share/cmake
+# will get
+#   CMAKE_MODULE_PATH
+# from xo-cmake-config --cmake-module-path
+#
 # and expect .cmake macros in
-#   /usr/local/share/cmake/xo_macros/xo-project-macros.cmake
+#   CMAKE_MODULE_PATH/xo_macros/xo_cxx.cmake
 # ----------------------------------------------------------------
 
 find_program(XO_CMAKE_CONFIG_EXECUTABLE NAMES xo-cmake-config REQUIRED)
@@ -17,24 +19,17 @@ endif()
 
 message(STATUS "XO_CMAKE_CONFIG_EXECUTABLE=${XO_CMAKE_CONFIG_EXECUTABLE}")
 
-if (("${CMAKE_MODULE_PATH}" STREQUAL "") OR ("${CMAKE_MODULE_PATH}" STREQUAL prefix))
-    message(STATUS "call XO_CMAKE_CONFIG_EXECUTABLE to get XO_CMAKE_MODULE_PATH")
-    # default to typical install location for xo-project-macros
-    execute_process(COMMAND ${XO_CMAKE_CONFIG_EXECUTABLE} --cmake-module-path OUTPUT_VARIABLE XO_CMAKE_MODULE_PATH)
-    message(STATUS "XO_CMAKE_MODULE_PATH=[${XO_CMAKE_MODULE_PATH}]")
-    set(CMAKE_MODULE_PATH ${XO_CMAKE_MODULE_PATH})
-else()
-    message(STATUS "use already-defined CMAKE_MODULE_PATH=[${CMAKE_MODULE_PATH}]")
-endif()
-
 if (NOT XO_SUBMODULE_BUILD)
-    message(STATUS "CMAKE_MODULE_PATH=${CMAKE_MODULE_PATH}")
+    if (("${CMAKE_MODULE_PATH}" STREQUAL "") OR ("${CMAKE_MODULE_PATH}" STREQUAL prefix))
+        # default to typical install location for xo-project-macros
+        execute_process(COMMAND ${XO_CMAKE_CONFIG_EXECUTABLE} --cmake-module-path OUTPUT_VARIABLE CMAKE_MODULE_PATH)
+        message(STATUS "CMAKE_MODULE_PATH=${XO_CMAKE_MODULE_PATH}")
+    endif()
 endif()
 
 # needs to have been installed somewhere on CMAKE_MODULE_PATH,
 # (e.g. from xo-cmake with the same value for CMAKE_INSTALL_PREFIX)
 #
-#include(xo_macros/xo-project-macros)
-include(xo_macros/xo_cxx)   # not using v1 code-coverage; testing cmake-examples impl instead
+include(xo_macros/xo_cxx)
 
 xo_cxx_bootstrap_message()
