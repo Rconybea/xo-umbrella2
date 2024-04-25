795f680c27c91f22dd9c0e7d460b12cdc0b32127
Roland Conybeare <rconybeare@gmail.com>
2024-04-25 13:24:10 -0400

    xo-flatstring: build: debugging cmake bootstrap

--- cmake/xo-bootstrap-macros.cmake
+++ cmake/xo-bootstrap-macros.cmake
@@ -18,8 +18,11 @@ endif()
 message(STATUS "XO_CMAKE_CONFIG_EXECUTABLE=${XO_CMAKE_CONFIG_EXECUTABLE}")
 
 if (("${CMAKE_MODULE_PATH}" STREQUAL "") OR ("${CMAKE_MODULE_PATH}" STREQUAL prefix))
+    message(STATUS "call XO_CMAKE_CONFIG_EXECUTABLE to get CMAKE_MODULE_PATH");
     # default to typical install location for xo-project-macros
     execute_process(COMMAND ${XO_CMAKE_CONFIG_EXECUTABLE} --cmake-module-path OUTPUT_VARIABLE CMAKE_MODULE_PATH)
+else()
+    message(STATUS "use already-defined CMAKE_MODULE_PATH=[${CMAKE_MODULE_PATH}]")
 endif()
 
 if (NOT XO_SUBMODULE_BUILD)
