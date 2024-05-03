dd643109ccd48faa594a156fd39d9e281d7953a9
Roland Conybeare <rconybeare@gmail.com>
2024-05-03 18:45:37 -0400

    xo-unit: streamlined ex2

--- example/ex2/ex2.cpp
+++ example/ex2/ex2.cpp
@@ -1,20 +1,41 @@
 /** @file ex2.cpp **/
 
-#include "xo/unit/mpl/quantity.hpp"
+#include "xo/unit/quantity.hpp"
+#include "xo/unit/quantity_iostream.hpp"
 #include <iostream>
 
 int
 main () {
-    namespace u = xo::unit::units;
-    namespace qty = xo::unit::qty;
-    using xo::unit::quantity;
+    namespace q = xo::qty::qty;
+    namespace su = xo::qty::su;
+    using xo::qty::with_units;
+    using xo::qty::quantity;
+    using xo::flatstring;
     using namespace std;
 
-    quantity<u::second> t = qty::milliseconds(10);
-    quantity<u::gram> m = qty::kilograms(2.5);
-    auto a = m / (t*t);
+    constexpr auto t = q::minutes(2);
+    constexpr auto d = q::kilometers(2.5);
 
-    cerr << "t: " << t << ", m: " << m << ", m.t^-2: " << a << endl;
+    constexpr auto t2 = t*t;
+    constexpr auto a = d / (t*t);
+
+    cerr << "t: " << t << ", d: " << d
+         << ", t^2: " << t2
+         << ", d.t^-2: " << a
+         << endl;
+
+    constexpr auto a2 = a.template rescale<su::meter / (su::second * su::second)>();
+
+    static_assert(a2.abbrev() == flatstring("m.s^-2"));
+
+    cerr << "a2: " << a2 << endl;
+
+    constexpr auto au = q::meter / (q::second * q::second);
+    constexpr auto a3 = with_units(a, au);
+
+    static_assert(a3.abbrev() == flatstring("m.s^-2"));
+
+    cerr << "a3: " << a3 << endl;
 }
 
 /** end ex2.cpp **/
