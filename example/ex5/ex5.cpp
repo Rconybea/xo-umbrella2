3b2fa29821c83fbd30031abc514e710a8860ddda
Roland Conybeare <rconybeare@gmail.com>
2024-05-20 07:31:21 -0400

    xo-unit: fixup ex5 implementation

--- example/ex5/ex5.cpp
+++ example/ex5/ex5.cpp
@@ -1,18 +1,19 @@
 /** @file ex5.cpp **/
 
-#include "xo/unit/mpl/quantity.hpp"
+#include "xo/unit/quantity.hpp"
+#include "xo/unit/quantity_iostream.hpp"
 #include <iostream>
 
 int
 main () {
-    namespace u = xo::unit::units;
-    namespace qty = xo::unit::qty;
+    //namespace u = xo::unit::units;
+    namespace q = xo::qty::qty;
     using namespace std;
 
     /* 20% volatility over 250 days (approx number of trading days in one year) */
-    auto q1 = qty::volatility250d(0.2);
+    auto q1 = q::volatility_250d(0.2);
     /* 10% volatility over 30 days */
-    auto q2 = qty::volatility30d(0.1);
+    auto q2 = q::volatility_30d(0.1);
 
     auto sum = q1 + q2;
     auto prod = q1 * q2;
