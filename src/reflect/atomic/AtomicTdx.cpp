188825d449dff635b2d30069bc504b9adf19f050
Roland Conybeare <rconybeare@gmail.com>
2024-06-14 10:54:35 -0400

    xo-reflect: cosmetic: code layout

--- src/reflect/atomic/AtomicTdx.cpp
+++ src/reflect/atomic/AtomicTdx.cpp
@@ -4,21 +4,22 @@
 #include "TaggedPtr.hpp"
 
 namespace xo {
-  namespace reflect {
-    std::unique_ptr<AtomicTdx> AtomicTdx::make() {
-      return std::unique_ptr<AtomicTdx>(new AtomicTdx());
-    } /*make*/
+    namespace reflect {
+        std::unique_ptr<AtomicTdx>
+        AtomicTdx::make() {
+            return std::unique_ptr<AtomicTdx>(new AtomicTdx());
+        } /*make*/
 
-     TaggedPtr
-     AtomicTdx::child_tp(uint32_t /*i*/, void * /*object*/) const {
-       return TaggedPtr::universal_null();
-     } /*child_tp*/
+        TaggedPtr
+        AtomicTdx::child_tp(uint32_t /*i*/, void * /*object*/) const {
+            return TaggedPtr::universal_null();
+        } /*child_tp*/
 
-    std::string const &
-    AtomicTdx::struct_member_name(uint32_t i) const {
-      return TypeDescrExtra::struct_member_name(i);
-    } /*struct_member_name*/
-  } /*namespace reflect*/
+        std::string const &
+        AtomicTdx::struct_member_name(uint32_t i) const {
+            return TypeDescrExtra::struct_member_name(i);
+        } /*struct_member_name*/
+    } /*namespace reflect*/
 } /*namespace xo*/
 
 /* end AtomicTdx.cpp */
