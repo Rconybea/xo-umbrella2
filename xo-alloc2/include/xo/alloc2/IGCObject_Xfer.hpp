198f46f1c44740f48d36abcccf342aa7d1f3f325
Roland Conybeare <rconybeare@gmail.com>
2025-12-13 22:20:29 -0500

    xo-alloc2 scaffold for ACollector + AGCObject facets. [WIP]

---
+++ xo-alloc2/include/xo/alloc2/IGCObject_Xfer.hpp
@@ -0,0 +1,62 @@
+/** @file IGCObject_Xfer.hpp
+ *
+ *  @author Roland Conybeare, Dec 2025
+ **/
+
+#pragma once
+
+#include "AGCObject.hpp"
+
+namespace xo {
+    namespace mm {
+        /** @class IGCObject_Xfer
+         *
+         *  Adapts typed GC object implementation @tparam IGCObject_DRepr
+         *  to type-erased @ref AGCObject interface
+         **/
+        template <typename DRepr, typename IGCObject_DRepr>
+        struct IGCObject_Xfer : public AGCObject {
+            using Impl = IGCObject_DRepr;
+            using size_type = AGCObject::size_type;
+
+            static const DRepr & _dcast(Copaque d) { return *(const DRepr *)d; }
+            static DRepr & _dcast(Opaque d) { return *(DRepr *)d; }
+
+            // from AGCObject
+
+            // const methods
+
+            int32_t _typeseq() const noexcept override { return s_typeseq; }
+            size_type shallow_size(Copaque d) const noexcept override {
+                return I::shallow_copy(_dcast(d));
+            }
+            Opaque * shallow_copy(Copaque d) const noexcept override {
+                return I::shallow_size(_dcast(d));
+            }
+
+            // non-const methods
+
+            size_type forward_children(Opaque d) const noexcept override {
+                return I::forward_children(d);
+            }
+
+        private:
+            using I = Impl;
+
+        public:
+            static int32_t s_typeseq;
+            static bool _valid;
+        };
+
+        template <typename DRepr, typename IGCObject_DRepr>
+        int32_t
+        IGCObject_Xfer<DRepr, IGCObject_DRepr>::s_typeseq = facet::typeseq::id<DRepr>();
+
+        template <typename DRepr, typename IGCObject_DRepr>
+        bool
+        IGCObject_Xfer<DRepr, IGCObject_DRepr>::_valid = facet::valid_facet_implementation<AGCObject, IGCObject_Xfer>();
+
+    } /*namespace mm*/
+} /*namespace xo*/
+
+/* end IGCObject_Xfer.hpp */
---
+++ xo-alloc2/include/xo/alloc2/gc/generation.hpp
@@ -0,0 +1,29 @@
+/** @file generation.hpp
+ *
+ *  @author Roland Conybeare, Dec 2025
+ **/
+
+#include <cstddef>
+#include <cstdint>
+
+namespace xo {
+    namespace mm {
+        /** hard maximum number of generations **/
+        static constexpr uint32_t c_max_generation = 16;
+
+        /** @class generation
+         *  @brief type-safe generation number
+         **/
+        struct generation {
+            using value_type = std::uint32_t;
+
+            explicit generation(value_type x) : value_{x} {}
+
+            operator value_type() const { return value_; }
+
+            std::uint32_t value_;
+        };
+    }
+}
+
+/* end generation.hpp */
