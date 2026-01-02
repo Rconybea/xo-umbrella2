39a16a3f15fd870dc151d0a323cb0907c901365a
Roland Conybeare <rconybeare@gmail.com>
2026-01-02 18:55:53 -0500

    xo-object2: utest: ++ allocation in collector utest

--- include/xo/object2/DFloat.hpp
+++ include/xo/object2/DFloat.hpp
@@ -5,10 +5,29 @@
 
 #pragma once
 
+#include <xo/alloc2/Allocator.hpp>
+
 namespace xo {
     namespace scm {
-        using DFloat = double;
-    } /*nmaespace obj*/
+        struct DFloat {
+            using AAllocator = xo::mm::AAllocator;
+
+            explicit DFloat(double x) : value_{x} {}
+
+            /** allocate boxed value @p x using memory from @p mm **/
+            static DFloat * make(obj<AAllocator> mm,
+                                 double x);
+
+            double value() const noexcept { return value_; }
+
+            operator double() const noexcept { return value_; }
+
+        private:
+
+            /** boxed floating-oint value **/
+            double value_;
+        };
+    } /*nmaespace scm*/
 } /*namespace xo*/
 
 /* end DFloat.hpp */
