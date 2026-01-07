dd39ffc8d9a49f0de3b42b7b50a7085ad2654808
Roland Conybeare <rconybeare@gmail.com>
2026-01-07 12:27:15 -0500

    xo-array: + DArenaHashMap.insert

--- utest/DArenaHashMap.test.cpp
+++ utest/DArenaHashMap.test.cpp
@@ -7,6 +7,7 @@
 #include <catch2/catch.hpp>
 
 namespace xo {
+    using xo::mm::DArenaHashMapUtil;
     using xo::mm::DArenaHashMap;
     //using xo::mM::ArenaConfig;
 
@@ -18,7 +19,22 @@ namespace xo {
             HashMap map;
 
             REQUIRE(map.empty());
-            REQUIRE(map.capacity() == HashMap::group_size());
+            REQUIRE(map.size() == 0);
+            REQUIRE(map.groups() == 1);
+            REQUIRE(map.capacity() == DArenaHashMapUtil::c_group_size);
+        }
+
+        TEST_CASE("DArenaHashMap-ctor2", "[arena][DArenaHashMap]")
+        {
+            using HashMap = DArenaHashMap<int, int>;
+
+            HashMap map(257);
+
+            REQUIRE(map.empty());
+            REQUIRE(map.size() == 0);
+            REQUIRE(map.capacity() == map.groups() * DArenaHashMapUtil::c_group_size);
+            REQUIRE(map.capacity() == std::max(512ul,
+                                               DArenaHashMapUtil::c_group_size));
         }
     }
 }
