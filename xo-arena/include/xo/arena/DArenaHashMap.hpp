67552d90bd5a0b07beae1fe056e8ad5d77ab6da8
Roland Conybeare <rconybeare@gmail.com>
2026-01-07 17:56:38 -0500

    xo-indentlog xo-arena: improve verify_ok logging workflow
    + scope.retroactively_enable()

--- xo-arena/include/xo/arena/DArenaHashMap.hpp
+++ xo-arena/include/xo/arena/DArenaHashMap.hpp
@@ -6,12 +6,50 @@
 #pragma once
 
 #include "DArenaVector.hpp"
+#include <xo/indentlog/scope.hpp>
 #include <algorithm>
 #include <array>
 #include <utility>
 #include <cstring>
 
 namespace xo {
+    struct verify_policy {
+        static verify_policy log_only() {
+            return verify_policy{.flags_ = 0x01};
+        }
+        static verify_policy throw_only() {
+            return verify_policy{.flags_ = 0x02};
+        }
+        static verify_policy chatty() {
+            return verify_policy{.flags_ = 0x03};
+        }
+
+        bool is_silent() const noexcept { return flags_ == 0; }
+        bool log_flag() const noexcept { return flags_ & 0x01; }
+        bool throw_flag() const noexcept { return flags_ & 0x02; }
+
+        template<typename... Tn>
+        bool report_error(scope & log, Tn&&... args)
+        {
+            if (!this->is_silent()) {
+                // TODO: consider global arena here for string
+                std::string msg = tostr(std::forward<Tn>(args)...);
+
+                if (this->log_flag()) {
+                    log.retroactively_enable();
+                    log(msg);
+                }
+                if (this->throw_flag()) {
+                    throw std::runtime_error(msg);
+                }
+            }
+            return false;
+        }
+
+        const char * c_self_ = "anonymous";
+        uint8_t flags_;
+    };
+
     namespace mm {
 #ifdef NOT_YET
         enum class insert_error : int32_t {
@@ -169,7 +207,7 @@ namespace xo {
              **/
             std::pair<value_type *, bool> try_insert(const std::pair<const Key, Value> & kv_pair);
 
-            bool verify_ok(bool /*throw_flag_not_implemented*/ = true) const;
+            bool verify_ok(verify_policy p = verify_policy::throw_only()) const;
 
         private:
             /** load group abstraction from control bytes starting at @p ix **/
@@ -382,8 +420,23 @@ namespace xo {
          **/
         template <typename Key, typename Value, typename Hash, typename Equal>
         bool
-        DArenaHashMap<Key, Value, Hash, Equal>::verify_ok(bool /*throw_flag_not_implemented*/) const
+        DArenaHashMap<Key, Value, Hash, Equal>::verify_ok(verify_policy policy) const
         {
+            using xo::scope;
+            using xo::tostr;
+            using xo::xtag;
+
+            constexpr const char * c_self = "DArenaHashMap::verify_ok";
+            scope log(XO_DEBUG(debug_flag_), xtag("size", size_));
+
+            /* SM1.1: size_ <= n_slot_ */
+            if (size_ > n_slot_) {
+                return policy.report_error(log,
+                                           c_self, ": expect .size < .n_slot",
+                                           xtag("size", size_),
+                                           xtag("n_slot", n_slot_));
+            }
+
             return true;
         }
     }
