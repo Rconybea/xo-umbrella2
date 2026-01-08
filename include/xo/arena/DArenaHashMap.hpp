4ab0837c4d5ad07949388ed55b58e3a367c571e7
Roland Conybeare <rconybeare@gmail.com>
2026-01-08 18:42:51 -0500

    xo-arena: reorg: move verify_policy to dedicated file

--- include/xo/arena/DArenaHashMap.hpp
+++ include/xo/arena/DArenaHashMap.hpp
@@ -6,6 +6,7 @@
 #pragma once
 
 #include "DArenaVector.hpp"
+#include "hashmap/verify_policy.hpp"
 #include "hashmap/DArenaHashMapUtil.hpp"
 #include "hashmap/ControlGroup.hpp"
 #include <xo/indentlog/scope.hpp>
@@ -15,43 +16,6 @@
 #include <cstring>
 
 namespace xo {
-    struct verify_policy {
-        static verify_policy log_only() {
-            return verify_policy{.flags_ = 0x01};
-        }
-        static verify_policy throw_only() {
-            return verify_policy{.flags_ = 0x02};
-        }
-        static verify_policy chatty() {
-            return verify_policy{.flags_ = 0x03};
-        }
-
-        bool is_silent() const noexcept { return flags_ == 0; }
-        bool log_flag() const noexcept { return flags_ & 0x01; }
-        bool throw_flag() const noexcept { return flags_ & 0x02; }
-
-        template<typename... Tn>
-        bool report_error(scope & log, Tn&&... args)
-        {
-            if (!this->is_silent()) {
-                // TODO: consider global arena here for string
-                std::string msg = tostr(std::forward<Tn>(args)...);
-
-                if (this->log_flag()) {
-                    log.retroactively_enable();
-                    log(msg);
-                }
-                if (this->throw_flag()) {
-                    throw std::runtime_error(msg);
-                }
-            }
-            return false;
-        }
-
-        const char * c_self_ = "anonymous";
-        uint8_t flags_;
-    };
-
     namespace mm {
 #ifdef NOT_YET
         enum class insert_error : int32_t {
