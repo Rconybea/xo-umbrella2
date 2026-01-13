b5d69aefe06ecf9c0859dafad5573d5f0c200dae
Roland Conybeare <rconybeare@gmail.com>
2026-01-13 16:47:44 -0500

    xo-object2: add DString::from_cstr + utest

--- xo-object2/utest/DString.test.cpp
+++ xo-object2/utest/DString.test.cpp
@@ -6,6 +6,7 @@
 #include <xo/object2/DString.hpp>
 #include <xo/alloc2/arena/IAllocator_DArena.hpp>
 #include <catch2/catch.hpp>
+#include <cstring>
 
 namespace xo {
     using xo::scm::DString;
@@ -29,6 +30,22 @@ namespace xo {
             REQUIRE(s->capacity() == 16);
             REQUIRE(s->size() == 0);
             REQUIRE(s->chars()[0] == '\0');
+        }
+
+        TEST_CASE("DString-from_cstr", "[object2][DString]")
+        {
+            ArenaConfig cfg { .name_ = "testarena",
+                              .size_ = 4*1024 };
+            DArena arena = DArena::map(cfg);
+            auto alloc = with_facet<AAllocator>::mkobj(&arena);
+
+            const char * cstr = "hello world";
+            DString * s = DString::from_cstr(alloc, cstr);
+
+            REQUIRE(s != nullptr);
+            REQUIRE(s->capacity() == 12);
+            REQUIRE(s->size() == 11);
+            REQUIRE(std::strcmp(s->chars(), cstr) == 0);
         }
     } /*namespace ut*/
 } /*namespace xo*/
