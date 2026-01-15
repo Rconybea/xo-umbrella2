cb56e1ed266594ca7b9a10fedfae691f2f6e3b6d
Roland Conybeare <rconybeare@gmail.com>
2026-01-15 11:17:55 -0500

    xo-object2: + StringOps::from_cstr + utest

--- xo-object2/utest/StringOps.test.cpp
+++ xo-object2/utest/StringOps.test.cpp
@@ -26,7 +26,7 @@ namespace xo {
             DArena arena = DArena::map(cfg);
             auto alloc = with_facet<AAllocator>::mkobj(&arena);
 
-            obj<AGCObject, DString> s = StringOps::empty(alloc, 16);
+            auto s = StringOps::empty(alloc, 16);
 
             REQUIRE(s.data() != nullptr);
             REQUIRE(s.data()->capacity() == 16);
@@ -41,12 +41,28 @@ namespace xo {
             DArena arena = DArena::map(cfg);
             auto alloc = with_facet<AAllocator>::mkobj(&arena);
 
-            obj<AGCObject, DString> s = StringOps::empty(alloc, 32);
+            auto s = StringOps::empty(alloc, 32);
 
             s.data()->sprintf("hello %s %d", "world", 42);
 
             REQUIRE(s.data()->size() == 14);
             REQUIRE(std::strcmp(s.data()->chars(), "hello world 42") == 0);
+        }
+
+        TEST_CASE("StringOps-from_cstr", "[object2][StringOps]")
+        {
+            ArenaConfig cfg { .name_ = "testarena",
+                              .size_ = 4*1024 };
+            DArena arena = DArena::map(cfg);
+            auto alloc = with_facet<AAllocator>::mkobj(&arena);
+
+            const char * cstr = "hello world";
+            auto s = StringOps::from_cstr(alloc, cstr);
+
+            REQUIRE(s.data() != nullptr);
+            REQUIRE(s.data()->capacity() == 12);
+            REQUIRE(s.data()->size() == 11);
+            REQUIRE(std::strcmp(s.data()->chars(), cstr) == 0);
         }
     } /*namespace ut*/
 } /*namespace xo*/
