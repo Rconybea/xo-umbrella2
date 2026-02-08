44758bfed34567344ce67dcae8abba2703bb6cde
Roland Conybeare <rconybeare@gmail.com>
2026-02-08 01:01:03 -0500

    xo-interpreter2 stack: work on VSM for apply -> closure action [WIP]

---
+++ include/xo/object2/Array.hpp
@@ -0,0 +1,14 @@
+/** @file Array.hpp
+ *
+ *  @author Roland Conybeare, Feb 2026
+ **/
+
+#pragma once
+
+#include "DArray.hpp"
+#include "array/ISequence_DArray.hpp"
+#include "array/IGCObject_DArray.hpp"
+#include "array/IPrintable_DArray.hpp"
+
+
+/* end Array.hpp */
