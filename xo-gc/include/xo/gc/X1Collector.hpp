4c785e647e1d522072113578ee7d0b2de78c5910
Roland Conybeare <rconybeare@gmail.com>
2026-02-15 13:06:33 -0500

    xo-gc: + X1Collector.hpp convenience header

---
+++ xo-gc/include/xo/gc/X1Collector.hpp
@@ -0,0 +1,11 @@
+/** @file X1Collector.hpp
+ *
+ *  @author Roland Conybeare, Feb 2026
+ **/
+
+#pragma once
+
+#include "DX1Collector.hpp"
+#include "detail/ICollector_DX1Collector.hpp"
+
+/* end X1Collector.hpp */
