5fc59b0df445abed81133f58dbab2b419858bd3a
Roland Conybeare <rconybeare@gmail.com>
2026-03-05 00:50:58 +1100

    refactor: + xo-stringtable2 w/ DString impl

---
+++ include/xo/stringtable2/stringtable2_register_facets.hpp
@@ -0,0 +1,15 @@
+/** @file stringtable2_register_facets.hpp
+ *
+ *  @author Roland Conybeare, Mar 2026
+ **/
+
+#pragma once
+
+namespace xo {
+    namespace scm {
+        /** Register object2 (facet,impl) combinations with FacetRegistry **/
+        bool stringtable2_register_facets();
+    }
+}
+
+/* end stringtable2_register_facets.hpp */
---
+++ src/stringtable2/stringtable2_register_types.cpp
@@ -0,0 +1,33 @@
+/** @file stringtable2_register_types.cpp
+ *
+ *  @author Roland Conybeare, Mar 2026
+ **/
+
+#include "stringtable2_register_types.hpp"
+#include "String.hpp"
+
+//#include <xo/facet/FacetRegistry.hpp>
+#include <xo/indentlog/scope.hpp>
+
+namespace xo {
+    using xo::mm::ACollector;
+    using xo::mm::AGCObject;
+    using xo::facet::impl_for;
+    using xo::scope;
+
+    namespace scm {
+        bool
+        stringtable2_register_types(obj<ACollector> gc)
+        {
+            scope log(XO_DEBUG(true));
+
+            bool ok = true;
+
+            ok &= gc.install_type(impl_for<AGCObject, DString>());
+
+            return ok;
+        }
+    }
+} /*namespace xo*/
+
+/* end stringtable2_register_types.cpp */
