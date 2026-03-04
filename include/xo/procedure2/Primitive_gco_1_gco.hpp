dbf43f009b57fb141b216d2f9022f9670574e721
Roland Conybeare <rconybeare@gmail.com>
2026-03-04 14:18:50 +1100

    xo-interpreter2 stack: + DPrimitive_gco_1_gco [foundation]

---
+++ include/xo/procedure2/Primitive_gco_1_gco.hpp
@@ -0,0 +1,11 @@
+/** @file Primitive_gco_1_gco.hpp
+ *
+ *  @author Roland Conybeare, Feb 2026
+ **/
+
+#include "DPrimitive_gco_1_gco.hpp"
+#include "detail/IProcedure_DPrimitive_gco_1_gco.hpp"
+#include "detail/IGCObject_DPrimitive_gco_1_gco.hpp"
+#include "detail/IPrintable_DPrimitive_gco_1_gco.hpp"
+
+/* end Primitive_gco_1_gco.hpp */
