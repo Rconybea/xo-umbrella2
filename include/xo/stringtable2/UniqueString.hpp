f60f90d8f321ec2ef545b9708ac654613d9d14ef
Roland Conybeare <rconybeare@gmail.com>
2026-03-05 13:02:12 +1100

    xo-interpreter2 stack: refactor: string clases -> xo-stringtable2/

---
+++ include/xo/stringtable2/UniqueString.hpp
@@ -0,0 +1,12 @@
+/** @file UniqueString.hpp
+ *
+ *  @author Roland Conybeare, Feb 2026
+ **/
+
+#pragma once
+
+#include "DUniqueString.hpp"
+#include "uniquestring/IGCObject_DUniqueString.hpp"
+#include "uniquestring/IPrintable_DUniqueString.hpp"
+
+/* end UniqueString.hpp */
--- include/xo/stringtable2/uniquestring/IGCObject_DUniqueString.hpp
+++ include/xo/stringtable2/uniquestring/IGCObject_DUniqueString.hpp
@@ -1,14 +1,14 @@
-/** @file IGCObject_DString.hpp
+/** @file IGCObject_DUniqueString.hpp
  *
  *  Generated automagically from ingredients:
  *  1. code generator:
  *       [xo-facet/codegen/genfacet]
  *     arguments:
- *       --input [idl/IGCObject_DString.json5]
+ *       --input [idl/IGCObject_DUniqueString.json5]
  *  2. jinja2 template for abstract facet .hpp file:
  *       [iface_facet_repr.hpp.j2]
  *  3. idl for facet methods
- *       [idl/IGCObject_DString.json5]
+ *       [idl/IGCObject_DUniqueString.json5]
  **/
 
 #pragma once
@@ -16,30 +16,30 @@
 #include "GCObject.hpp"
 #include <xo/alloc2/GCObject.hpp>
 #include <xo/alloc2/Allocator.hpp>
-#include "DString.hpp"
+#include "DUniqueString.hpp"
 
-namespace xo { namespace scm { class IGCObject_DString; } }
+namespace xo { namespace scm { class IGCObject_DUniqueString; } }
 
 namespace xo {
     namespace facet {
         template <>
         struct FacetImplementation<xo::mm::AGCObject,
-                                   xo::scm::DString>
+                                   xo::scm::DUniqueString>
         {
             using ImplType = xo::mm::IGCObject_Xfer
-              <xo::scm::DString,
-               xo::scm::IGCObject_DString>;
+              <xo::scm::DUniqueString,
+               xo::scm::IGCObject_DUniqueString>;
         };
     }
 }
 
 namespace xo {
     namespace scm {
-        /** @class IGCObject_DString
+        /** @class IGCObject_DUniqueString
          **/
-        class IGCObject_DString {
+        class IGCObject_DUniqueString {
         public:
-            /** @defgroup scm-gcobject-dstring-type-traits **/
+            /** @defgroup scm-gcobject-duniquestring-type-traits **/
             ///@{
             using size_type = xo::mm::AGCObject::size_type;
             using AAllocator = xo::mm::AGCObject::AAllocator;
@@ -47,17 +47,17 @@ namespace xo {
             using Copaque = xo::mm::AGCObject::Copaque;
             using Opaque = xo::mm::AGCObject::Opaque;
             ///@}
-            /** @defgroup scm-gcobject-dstring-methods **/
+            /** @defgroup scm-gcobject-duniquestring-methods **/
             ///@{
             // const methods
             /** memory consumption for this instance **/
-            static size_type shallow_size(const DString & self) noexcept;
+            static size_type shallow_size(const DUniqueString & self) noexcept;
             /** copy instance using allocator **/
-            static Opaque shallow_copy(const DString & self, obj<AAllocator> mm) noexcept;
+            static Opaque shallow_copy(const DUniqueString & self, obj<AAllocator> mm) noexcept;
 
             // non-const methods
             /** during GC: forward immdiate children **/
-            static size_type forward_children(DString & self, obj<ACollector> gc) noexcept;
+            static size_type forward_children(DUniqueString & self, obj<ACollector> gc) noexcept;
             ///@}
         };
 
