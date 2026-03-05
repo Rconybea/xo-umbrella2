af95656cc82694004fd3dace3733412665abd23d
Roland Conybeare <rconybeare@gmail.com>
2026-03-05 20:11:50 +1100

    nix build: + xo-stringtable2

---
+++ pkgs/xo-stringtable2.nix
@@ -0,0 +1,34 @@
+{
+  # nixpkgs dependencies
+  stdenv, cmake, catch2,
+
+  # xo dependencies
+  xo-alloc2,
+  xo-printable2,
+  xo-arena,
+  xo-cmake,
+#  xo-reflectutil,
+#  xo-indentlog,
+} :
+
+stdenv.mkDerivation (finalattrs:
+  {
+    name = "xo-stringtable2";
+
+    src = ../xo-stringtable2;
+
+    cmakeFlags = ["-DCMAKE_MODULE_PATH=${xo-cmake}/share/cmake"
+                  "-DENABLE_TESTING=1"
+                 ];
+    doCheck = true;
+    nativeBuildInputs = [
+      cmake catch2 xo-cmake
+    ];
+    propagatedBuildInputs = [
+      xo-alloc2
+      xo-printable2
+      xo-arena
+#      xo-reflectutil
+#      xo-indentlog
+    ];
+  })
