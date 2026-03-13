c77a1d3f437c224ed5b363191196d28b08f340fd
Roland Conybeare <rconybeare@gmail.com>
2026-03-12 20:43:43 -0500

    xo-type: + DTypeVarRef

---
+++ xo-type/include/xo/type/typevar/IGCObject_DTypeVarRef.hpp
@@ -0,0 +1,65 @@
+/** @file IGCObject_DTypeVarRef.hpp
+ *
+ *  Generated automagically from ingredients:
+ *  1. code generator:
+ *       [xo-facet/codegen/genfacet]
+ *     arguments:
+ *       --input [idl/IGCObject_DTypeVarRef.json5]
+ *  2. jinja2 template for abstract facet .hpp file:
+ *       [iface_facet_repr.hpp.j2]
+ *  3. idl for facet methods
+ *       [idl/IGCObject_DTypeVarRef.json5]
+ **/
+
+#pragma once
+
+#include "GCObject.hpp"
+#include "DTypeVarRef.hpp"
+
+namespace xo { namespace scm { class IGCObject_DTypeVarRef; } }
+
+namespace xo {
+    namespace facet {
+        template <>
+        struct FacetImplementation<xo::mm::AGCObject,
+                                   xo::scm::DTypeVarRef>
+        {
+            using ImplType = xo::mm::IGCObject_Xfer
+              <xo::scm::DTypeVarRef,
+               xo::scm::IGCObject_DTypeVarRef>;
+        };
+    }
+}
+
+namespace xo {
+    namespace scm {
+        /** @class IGCObject_DTypeVarRef
+         **/
+        class IGCObject_DTypeVarRef {
+        public:
+            /** @defgroup scm-gcobject-dtypevarref-type-traits **/
+            ///@{
+            using size_type = xo::mm::AGCObject::size_type;
+            using AAllocator = xo::mm::AGCObject::AAllocator;
+            using ACollector = xo::mm::AGCObject::ACollector;
+            using Copaque = xo::mm::AGCObject::Copaque;
+            using Opaque = xo::mm::AGCObject::Opaque;
+            ///@}
+            /** @defgroup scm-gcobject-dtypevarref-methods **/
+            ///@{
+            // const methods
+            /** memory consumption for this instance **/
+            static size_type shallow_size(const DTypeVarRef & self) noexcept;
+            /** copy instance using allocator **/
+            static Opaque shallow_copy(const DTypeVarRef & self, obj<AAllocator> mm) noexcept;
+
+            // non-const methods
+            /** during GC: forward immdiate children **/
+            static size_type forward_children(DTypeVarRef & self, obj<ACollector> gc) noexcept;
+            ///@}
+        };
+
+    } /*namespace scm*/
+} /*namespace xo*/
+
+/* end */
---
+++ xo-type/src/type/DTypeVarRef.cpp
@@ -0,0 +1,103 @@
+/** @file DTypeVarRef.cpp
+ *
+ *  @author Roland Conybeare, Mar 2026
+ **/
+
+#include "Metatype.hpp"
+#include "TypeVarRef.hpp"
+#include <xo/reflect/Reflect.hpp>
+#include <xo/facet/FacetRegistry.hpp>
+
+namespace xo {
+    using xo::mm::AGCObject;
+    using xo::reflect::Reflect;
+    using xo::reflect::TypeDescr;
+    using xo::facet::FacetRegistry;
+
+    namespace scm {
+
+        DTypeVarRef *
+        DTypeVarRef::_make(obj<AAllocator> mm, const DUniqueString * name)
+        {
+            void * mem = mm.alloc_for<DTypeVarRef>();
+
+            return new (mem) DTypeVarRef(name);
+        }
+
+        obj<AType,DTypeVarRef>
+        DTypeVarRef::make(obj<AAllocator> mm, const DUniqueString * name)
+        {
+            return obj<AType,DTypeVarRef>(_make(mm, name));
+        }
+
+        // ----- Type facet -----
+
+        Metatype
+        DTypeVarRef::metatype() const noexcept
+        {
+            if (type_) {
+                // resolved typevar reference
+
+                return type_.metatype();
+            } else {
+                // type var reference, not yet resolved
+
+                return Metatype::t_any();
+            }
+        }
+
+        TypeDescr
+        DTypeVarRef::repr_td() const noexcept
+        {
+            return Reflect::require<void *>();
+        }
+
+        bool
+        DTypeVarRef::is_equal_to(const obj<AType> & y) const noexcept
+        {
+            // not really well-defined until types are resolved
+
+            return (this->metatype().code() == y.metatype().code());
+        }
+
+        bool
+        DTypeVarRef::is_subtype_of(const obj<AType> & y) const noexcept
+        {
+            Metatype x_mtype = this->metatype();
+            Metatype y_mtype = y.metatype();
+
+            if (y_mtype.code() == Metatype::code::t_any)
+                return true;
+
+            return (x_mtype.code() == y_mtype.code());
+        }
+
+        // ----- GCObject facet -----
+
+        std::size_t
+        DTypeVarRef::shallow_size() const noexcept
+        {
+            return sizeof(DTypeVarRef);
+        }
+
+        DTypeVarRef *
+        DTypeVarRef::shallow_copy(obj<AAllocator> mm) const noexcept
+        {
+            return mm.std_copy_for(this);
+        }
+
+        std::size_t
+        DTypeVarRef::forward_children(obj<ACollector> gc) noexcept
+        {
+            {
+                auto e = FacetRegistry::instance().variant<AGCObject,AType>(type_);
+                gc.forward_inplace(e.iface(), (void **)&type_.data_);
+            }
+
+            return this->shallow_size();
+        }
+
+    } /*namespace scm*/
+} /*namespace xo*/
+
+/* end DTypeVarRef.cpp */
