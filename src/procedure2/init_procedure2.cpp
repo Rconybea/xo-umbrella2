15edbdd7553a0f4bb7cc36138217b896f9870da0
Roland Conybeare <rconybeare@gmail.com>
2026-03-16 15:45:47 -0500

    xo-procedure2: streamline setup

--- src/procedure2/init_procedure2.cpp
+++ src/procedure2/init_procedure2.cpp
@@ -5,27 +5,22 @@
 
 #include "init_procedure2.hpp"
 #include "init_primitives.hpp"
-#include "procedure2_register_facets.hpp"
-#include "procedure2_register_types.hpp"
-#include "procedure2_register_primitives.hpp"
-
+#include "SetupProcedure2.hpp"
 #include <xo/object2/init_object2.hpp>
 #include <xo/alloc2/CollectorTypeRegistry.hpp>
 
 namespace xo {
-    using xo::scm::procedure2_register_primitives;
-    using xo::scm::procedure2_register_facets;
-    using xo::scm::procedure2_register_types;
+    using xo::scm::SetupProcedure2;
     using xo::scm::PrimitiveRegistry;
     using xo::mm::CollectorTypeRegistry;
 
     void
     InitSubsys<S_procedure2_tag>::init()
     {
-        procedure2_register_facets();
+        SetupProcedure2::register_facets();
 
-        CollectorTypeRegistry::instance().register_types(&procedure2_register_types);
-        PrimitiveRegistry::instance().register_primitives(&procedure2_register_primitives);
+        CollectorTypeRegistry::instance().register_types(&SetupProcedure2::register_types);
+        PrimitiveRegistry::instance().register_primitives(&SetupProcedure2::register_primitives);
     }
 
     InitEvidence
