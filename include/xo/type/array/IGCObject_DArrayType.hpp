064d30de84ca005e2c9ab58acf6efa29c2cbd82b
Roland Conybeare <rconybeare@gmail.com>
2026-04-04 16:54:46 -0400

    refactor: void return type for Collector.forward_children()

--- include/xo/type/array/IGCObject_DArrayType.hpp
+++ include/xo/type/array/IGCObject_DArrayType.hpp
@@ -55,7 +55,7 @@ namespace xo {
             /** move instance using allocator **/
             static Opaque shallow_move(DArrayType & self, obj<ACollector> gc) noexcept;
             /** during GC: forward immdiate children **/
-            static size_type forward_children(DArrayType & self, obj<ACollector> gc) noexcept;
+            static void forward_children(DArrayType & self, obj<ACollector> gc) noexcept;
             ///@}
         };
 
--- include/xo/type/typevar/IGCObject_DTypeVarRef.hpp
+++ include/xo/type/typevar/IGCObject_DTypeVarRef.hpp
@@ -55,7 +55,7 @@ namespace xo {
             /** move instance using allocator **/
             static Opaque shallow_move(DTypeVarRef & self, obj<ACollector> gc) noexcept;
             /** during GC: forward immdiate children **/
-            static size_type forward_children(DTypeVarRef & self, obj<ACollector> gc) noexcept;
+            static void forward_children(DTypeVarRef & self, obj<ACollector> gc) noexcept;
             ///@}
         };
 
