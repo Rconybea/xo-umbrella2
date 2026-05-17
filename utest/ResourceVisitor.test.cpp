9cf74ea149532fef897dd67d26f6142b98e452c8
Roland Conybeare <rconybeare@gmail.com>
2026-05-17 12:30:09 -0400

    xo-gc stack: many small utest improvements.

---
+++ utest/ResourceVisitor.test.cpp
@@ -0,0 +1,25 @@
+/** @file ResourceVisitor.test.cpp
+ *
+ *  @author Roland Conybeare, May 2026
+ **/
+
+#include <xo/alloc2/ResourceVisitor.hpp>
+#include <catch2/catch.hpp>
+
+namespace xo {
+    using xo::mm::AResourceVisitor;
+
+    namespace ut {
+
+        TEST_CASE("ResourceVisitor-1", "[resourcevisitor]")
+        {
+            obj<AResourceVisitor> v;
+
+            REQUIRE(v.iface());
+            REQUIRE(!v.iface()->_has_null_vptr());
+        }
+
+    }
+} /*namespace xo*/
+
+/* end ResourceVisitor.test.cpp */
