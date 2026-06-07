c8653b0b3839bca435d9d5e140b5b348206b793e
Roland Conybeare <rconybeare@gmail.com>
2026-06-06 22:25:50 -0400

    .xo-tokenizer subrepo tidy

--- .xo-tokenizer/src/tokenizer/token.cpp
+++
@@ -1,9 +0,0 @@
-/** @file token.cpp
- *
- *  author: Roland Conybeare
- **/
-
-#include "token.hpp"
-#include <xo/indentlog/print/tag.hpp>
-
-/** end token.cpp **/
--- .xo-tokenizer/utest/tokenizer_utest_main.cpp
+++
@@ -1,6 +0,0 @@
-/* file tokenizer_utest_main.cpp */
-
-#define CATCH_CONFIG_MAIN
-#include "catch2/catch.hpp"
-
-/* end tokenizer_utest_main.cpp */
