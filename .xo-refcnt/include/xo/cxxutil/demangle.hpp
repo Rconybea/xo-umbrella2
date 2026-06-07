304f7d8e8e54426ef943ddb0a33afd6e9216fe7d
Roland Conybeare <rconybeare@gmail.com>
2026-06-06 22:18:44 -0400

    .xo-refcnt subrepo tidy

--- .xo-refcnt/include/xo/cxxutil/demangle.hpp
+++
@@ -1,7 +0,0 @@
-/* @file demangle.hpp */
-
-#pragma once
-
-#include <xo/reflectutil/type_name.hpp>
-
-/* end demangle.hpp */
--- .xo-refcnt/utest/refcnt_utest_main.cpp
+++
@@ -1,6 +0,0 @@
-/* @file refcnt_utest_main.cpp */
-
-#define CATCH_CONFIG_MAIN
-#include "catch2/catch.hpp"
-
-/* end refcnt_utest_main.cpp */
