9176253d002b33619c7761fbdef31818aecdad9f
Roland Conybeare <rconybeare@gmail.com>
2026-07-19 17:19:47 -0400

    xo-indentlog2: + missing file [BUGFIX]

---
+++ xo-indentlog2/src/indentlog2/LogConfig.cpp
@@ -0,0 +1,16 @@
+/** @file LogConfig.cpp
+ *
+ *  @author Roland Conybeare, Jul 2026
+ **/
+
+#include "LogConfig.hpp"
+
+namespace xo {
+    LogConfig
+    LogConfig::s_instance;
+
+    LogConfig::LogConfig() = default;
+
+} /*namespace xo*/
+
+/* end LogConfig.cpp */
