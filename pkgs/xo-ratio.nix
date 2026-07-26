2d78e41f75d665460cc5151a7711bc8c5016e4b5
Roland Conybeare <rconybeare@gmail.com>
2026-07-26 13:27:23 -0400

    nix build: xo-ratio honors doCheck [NIX]

--- pkgs/xo-ratio.nix
+++ pkgs/xo-ratio.nix
@@ -1,6 +1,6 @@
 {
   # dependencies
-  lib, stdenv, cmake, #catch2,
+  lib, stdenv, cmake, catch2,
 
   python3Packages,
 
@@ -8,8 +8,12 @@
 
   xo-cmake, xo-flatstring, xo-reflectutil, xo-indentlog,
 
+  # test-only xo dependencies
+  xo-reflect, xo-randomgen,
+
   buildDocs ? false,
   buildExamples ? false,
+  doCheck ? true,
 } :
 
 stdenv.mkDerivation (finalattrs:
@@ -21,12 +25,13 @@ stdenv.mkDerivation (finalattrs:
 
     cmakeFlags = ["-DCMAKE_MODULE_PATH=${xo-cmake}/share/cmake"]
                  ++ lib.optionals buildDocs ["-DXO_ENABLE_DOCS=on"]
-                 ++ lib.optionals buildExamples ["-DXO_ENABLE_EXAMPLES=on"];
+                 ++ lib.optionals buildExamples ["-DXO_ENABLE_EXAMPLES=on"]
+                 ++ lib.optionals doCheck ["-DENABLE_TESTING=1"];
 
     inherit buildDocs;
     inherit buildExamples;
 
-    doCheck = true;
+    inherit doCheck;
 
     postBuild = lib.optionalString buildDocs ''
       cmake --build . -- docs
@@ -42,6 +47,12 @@ stdenv.mkDerivation (finalattrs:
     nativeBuildInputs = [
       cmake
       xo-cmake
+    ]
+    ++ lib.optionals doCheck [
+      catch2
+      xo-reflect
+      xo-randomgen
+      xo-indentlog
     ]
     ++ lib.optionals buildExamples [
       xo-indentlog
