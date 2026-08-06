5274c40d7e8368bb86e5e9f650b5120eee745042
Roland Conybeare <rconybeare@gmail.com>
2026-08-06 09:08:14 -0400

    xo-webutil: use xo-ppsink !xo-indentlog [REFACTOR]

--- xo-webutil/src/webutil/HttpEndpointDescr.cpp
+++ xo-webutil/src/webutil/HttpEndpointDescr.cpp
@@ -1,8 +1,8 @@
 /* @file HttpEndpointDescr.cpp */
 
 #include "HttpEndpointDescr.hpp"
-#include <xo/indentlog/print/tag.hpp>
-#include <xo/indentlog/print/tostr.hpp>
+#include <xo/ppsink/tag_ostream.hpp>   /* os << xtag(..) */
+#include <xo/ppsink/tostr.hpp>
 
 namespace xo {
     namespace web {
@@ -14,11 +14,25 @@ namespace xo {
 
         void
         HttpEndpointDescr::display(std::ostream & os) const {
+            /* function-local: a namespace-scope using here would become
+             * ambiguous (not shadowing) if any header in this TU ever makes
+             * legacy xo::xtag visible again.
+             */
+            using xo::pp::xtag;
+
             os << "<HttpEndpointDescr" << xtag("uri_pattern", uri_pattern_) << ">";
         } /*display*/
 
         std::string
-        HttpEndpointDescr::display_string() const { return tostr(*this); }
+        HttpEndpointDescr::display_string() const {
+            using xo::pp::tostr;
+
+            /* renders via operator<<(ostream&, HttpEndpointDescr) above --
+             * ppsink has no Prettifier for this type, so pretty() falls through
+             * to the operator<< path that tostr.hpp pulls in.
+             */
+            return tostr(*this);
+        }
     } /*namespace web*/
 
 } /*namespace xo*/
--- xo-webutil/src/webutil/StreamEndpointDescr.cpp
+++ xo-webutil/src/webutil/StreamEndpointDescr.cpp
@@ -1,8 +1,8 @@
 /* @file StreamEndpointDescr.cpp */
 
 #include "StreamEndpointDescr.hpp"
-#include <xo/indentlog/print/tag.hpp>
-#include <xo/indentlog/print/tostr.hpp>
+#include <xo/ppsink/tag_ostream.hpp>   /* os << xtag(..) */
+#include <xo/ppsink/tostr.hpp>
 
 namespace xo {
     namespace web {
@@ -16,11 +16,25 @@ namespace xo {
 
         void
         StreamEndpointDescr::display(std::ostream & os) const {
+            /* function-local: a namespace-scope using here would become
+             * ambiguous (not shadowing) if any header in this TU ever makes
+             * legacy xo::xtag visible again.
+             */
+            using xo::pp::xtag;
+
             os << "<StreamEndpointDescr" << xtag("uri_pattern", uri_pattern_) << ">";
         } /*display*/
 
         std::string
-        StreamEndpointDescr::display_string() const { return tostr(*this); }
+        StreamEndpointDescr::display_string() const {
+            using xo::pp::tostr;
+
+            /* renders via operator<<(ostream&, StreamEndpointDescr) above --
+             * ppsink has no Prettifier for this type, so pretty() falls through
+             * to the operator<< path that tostr.hpp pulls in.
+             */
+            return tostr(*this);
+        }
     } /*namespace web*/
 } /*namespace xo*/
 
